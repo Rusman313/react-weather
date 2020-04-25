ffbd37dae8b190996d4a6cc0db1eeb6750e4deed
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-04-25 06:59:14 -0400

    Information is showing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
-import { render } from "@testing-library/react";
-class App extends React.Component {
-    const key = "98714695d6d6eca2461e95c2aa78b140";
-    getWeather = async (e) => {};
+import "moment-timezone";
+import moment from "moment";
+import tz from "zipcode-to-timezone";
+
+var Api_key = "98714695d6d6eca2461e95c2aa78b140";
+
+class App extends Component {
+  state = {}; //leave blank - argument that you pass through
+
+  getWeather = () => {
+    const zip = document.getElementById("zip").value; //gets the value of the zip input from the input field
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?zip=" +
+        zip +
+        ",us&appid=" +
+        Api_key +
+        "&units=imperial" // gets the api data with the zip input and converts to imperial
+    ).then((response) => {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then((data) => {
+        console.log(data);
+        this.setState({
+          city: data.name,
+          timezone: data.timezone,
+          theweather: data.weather[0].main,
+          icon: data.weather.icon,
+          temp: data.main.temp + "°F",
+          wind: data.wind.speed,
+        });
+      });
+    });
+  };
 
   render() {
     return (
@@ -16,7 +46,10 @@ class App extends React.Component {
               <div className="card-body">
                 <div className="input">
                   <input type="text" id="zip" className="card-text" />
-                  <button id="getWeather">Get Weather</button>
+                  <button id="getWeather" onClick={this.getWeather}>
+                    {/* on click of the button get "this function's response"*/}
+                    Get Weather
+                  </button>
                   <h5 id="showWeatherForecast" className="card-title"></h5>
                   <h6 id="time" className="card-title"></h6>
                 </div>
