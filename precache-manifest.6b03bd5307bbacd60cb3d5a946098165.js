c2f62478fe5ccf3ec925d271dfa237de118bbe09
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-04-26 14:03:25 -0400

    Updates

---
+++ precache-manifest.6b03bd5307bbacd60cb3d5a946098165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921c7c228cb60d57f7774bfaeeacd0b1",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "07337a727cf60911ef74",
+    "url": "/react-weather/static/css/main.1bff57b6.chunk.css"
+  },
+  {
+    "revision": "a1fee4a174c3b17f24ec",
+    "url": "/react-weather/static/js/2.5e1a3dfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.5e1a3dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07337a727cf60911ef74",
+    "url": "/react-weather/static/js/main.c139b26d.chunk.js"
+  },
+  {
+    "revision": "b8670151fa5f6c40bcc4",
+    "url": "/react-weather/static/js/runtime-main.b6c39b94.js"
+  }
+]);
