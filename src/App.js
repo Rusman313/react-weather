5633466cc4e582e87236a70beff199917d95e1ea
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-04-26 12:55:37 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import "./App.css";
 import "moment-timezone";
 import moment from "moment";
+import format from "moment";
 import tz from "zipcode-to-timezone";
 
 var Api_key = "98714695d6d6eca2461e95c2aa78b140";
 
 class App extends Component {
   state = {}; //leave blank - argument that you pass through
+  getTZ = () => {
+    const zip = document.getElementById("zip").value; //gets the value of the zip input from the input field
+    const zone = tz.lookup(zip); //uses "zipcode-to-timezone" to find hte timezone of the zip that has been fetched
+    console.log(zone);
+    const time = moment().tz(zone);
+    console.log(time);
+    this.setState({
+      time: moment._d,
+    });
+  };
 
   getWeather = () => {
     const zip = document.getElementById("zip").value; //gets the value of the zip input from the input field
@@ -28,10 +39,12 @@ class App extends Component {
           city: data.name,
           timezone: data.timezone,
           theweather: data.weather[0].main,
-          icon: data.weather.icon,
           temp: data.main.temp + "°F",
           wind: data.wind.speed,
+          min: data.main.temp_min,
+          max: data.main.temp_max,
         });
+        this.getTZ(); // calls the tz function so that it both will run when button is clicked
       });
     });
   };
@@ -50,7 +63,10 @@ class App extends Component {
                     {/* on click of the button get "this function's response"*/}
                     Get Weather
                   </button>
-                  <h5 id="showWeatherForecast" className="card-title"></h5>
+                  <h5 id="showWeatherForecast" className="card-title">
+                    ({this.state.city} + {this.state.temp})
+                  </h5>
+
                   <h6 id="time" className="card-title"></h6>
                 </div>
               </div>
